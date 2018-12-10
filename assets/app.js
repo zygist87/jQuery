681356eca38c1e55628dc9f695b3f2fd00602719
zygist87 <zygist87@gmail.com>
2018-12-10 06:36:54 +0200

    first

--- assets/app.js
+++ assets/app.js
@@ -1,81 +1,49 @@
 console.log('bandymas');
-$(".one").hide();
 $(document).ready(function(){
-  $('button').click(function(){
-    $(".two").hide();
+  $("#btn1").click(function(){
+    alert("Text:" + $("#test").text());
   });
-});
-$(document).ready(function(){
-  $(".secondButton").click(function(){
-    $(".three").hide();
+  $("#btn2").click(function(){
+    alert("HTML:" + $('#test').html());
   });
-});
-/*
-$(document).ready(function(){
-  $(".three").mouseenter(function(){
-    alert('What the fuck');
+  $('#value1').click(function(){
+    alert("value:" + $("#test").val());
   });
-});
-*/
-$(document).ready(function(){
-  $("h2").focus(function(){
-    $("h2").css("color", "red");
-  });
-});
-
-
-$(document).ready(function(){
-    $("input").focus(function(){
-        $(this).css("background-color", "#cccccc");
-    });
-    $("input").blur(function(){
-        $(this).css("background-color", "red");
-    });
-});
-
-$(document).ready(function(){
-  $("p").on("click", function(){
-    $("p").hide();
+  $('#adresas').click(function(){
+    alert($("#w3s").attr('href'));
   });
 });
 
 $(document).ready(function(){
-  $("#hide").click(function(){
-    $("p").hide(1000);
-  })
-  $("#show").click(function(){
-    $("p").show(100);
+  $("#btn4").click(function(){
+    $("#test3").text("Ka tu, sepeti?");
+  });
+  $("#btn6").click(function(){
+    $("#test5").val("e, nepjauk grybo");
+  });
+});
+
+$(document).ready(function(){
+  $("#appenddd").click(function(){
+    $("#apppendT").append("bam bam bam");
+  });
+});
+
+$(document).ready(function(){
+  $("#liks").click(function(){
+    $("#dings").remove();
   });
 });
 
 $(document).ready(function(){
   $("button").click(function(){
-    $("p").toggle(100);
+    $("h1, h2, p").addClass("blue");
+    $("div").toggleClass("important");
   });
 });
 
 $(document).ready(function(){
-  $(".mk").click(function(){
-    $("#div1").fadeToggle("slow");
-    $("#div2").fadeToggle("fast");
-    $("#div3").fadeToggle(500);
-  });
-});
-
-$(document).ready(function(){
-  $(".iki").click(function(){
-    $("#div4").fadeTo("slow", 0.05);
-    $("#div5").fadeTo('fast', 0.1);
-    $("#div6").fadeTo(1);
-  });
-});
-$(document).ready(function(){
-  $(".menu").click(function(){
-    $(".listas").slideDown("slow");
-  });
-});
-$(document).ready(function(){
-  $(".listas").mouseenter(function(){
-    $(".listas").css("color", "red");
+  $("#btn7").click(function(){
+    $("#div7").width(500).height(500);
   });
 });
